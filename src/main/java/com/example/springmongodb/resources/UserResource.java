4fc0891e26cc231462cda32288725b1739766489
Brian dos Reis <brazdore@gmail.com>
2021-08-01 16:36:29 -0300

    MongoDB conn, Repository, Service

--- src/main/java/com/example/springmongodb/resources/UserResource.java
+++ src/main/java/com/example/springmongodb/resources/UserResource.java
@@ -1,24 +1,25 @@
 package com.example.springmongodb.resources;
 
 import com.example.springmongodb.domain.User;
+import com.example.springmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User kiara = new User("1", "Kiara", "phoenix@kfp.com");
-        User nenechi = new User("2", "Nenechi", "neneseal@gmail.com");
-        List<User> list = new ArrayList<>(Arrays.asList(kiara, nenechi));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
