ba6d87ced1698a1a0b0f2cab4d59df10e963f19c
Brian dos Reis <brazdore@gmail.com>
2021-08-02 00:32:02 -0300

    UserByID, ExceptionHandler

--- src/main/java/com/example/springmongodb/services/UserService.java
+++ src/main/java/com/example/springmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.example.springmongodb.services;
 
 import com.example.springmongodb.domain.User;
 import com.example.springmongodb.repositories.UserRepository;
+import com.example.springmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findByID(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("User not found. ID: " + id + "."));
     }
 }
