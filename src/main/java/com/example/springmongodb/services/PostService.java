36f1bf826e99b7b4dd0aea64c0aa0ee060a81017
Brian dos Reis <brazdore@gmail.com>
2021-08-02 18:36:16 -0300

    Query Methods findByTitle

--- src/main/java/com/example/springmongodb/services/PostService.java
+++ src/main/java/com/example/springmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.example.springmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findByID(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Post not found. ID: " + id + "."));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
